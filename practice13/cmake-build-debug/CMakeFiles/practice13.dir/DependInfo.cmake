e277fff52f915eacfa7798b42887d38bb595fafd
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-03-29 20:08:23 +0300

    13 лаба

---
+++ practice13/cmake-build-debug/CMakeFiles/practice13.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/tsiga/CLionProjects/project/practice13/list.c" "CMakeFiles/practice13.dir/list.c.obj" "gcc" "CMakeFiles/practice13.dir/list.c.obj.d"
+  "C:/Users/tsiga/CLionProjects/project/practice13/main.c" "CMakeFiles/practice13.dir/main.c.obj" "gcc" "CMakeFiles/practice13.dir/main.c.obj.d"
+  "C:/Users/tsiga/CLionProjects/project/practice13/stud.c" "CMakeFiles/practice13.dir/stud.c.obj" "gcc" "CMakeFiles/practice13.dir/stud.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
