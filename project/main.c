956c12457575a39a39ee476c4d6cfdb5de513634
Petya <tsigay.petya@mail.ru>
2024-03-29 15:06:20 +0300

    fgnf

---
+++ project/main.c
@@ -0,0 +1,14 @@
+//
+// Created by tsiga on 29.03.2024.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main()
+{
+    printf("Введите количljdnflkaklaество студентов: ");
+
+    return 0;
+}
