a36b6b158dcd43f8ad12b6781ec5de5bca5ed620
Petyaprog <165314326+Petyaprog@users.noreply.github.com>
2024-03-29 16:03:00 +0300

    dvomsdo

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/list.c.obj"
+  "CMakeFiles/untitled1.dir/list.c.obj.d"
+  "CMakeFiles/untitled1.dir/main.c.obj"
+  "CMakeFiles/untitled1.dir/main.c.obj.d"
+  "CMakeFiles/untitled1.dir/stud.c.obj"
+  "CMakeFiles/untitled1.dir/stud.c.obj.d"
+  "libuntitled1.dll.a"
+  "untitled1.exe"
+  "untitled1.exe.manifest"
+  "untitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
